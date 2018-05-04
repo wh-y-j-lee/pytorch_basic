d7029be1617970c6d2fea95ce7f48287e607fe4d
YongJu Lee <paulyongju@yonsei.ac.kr>
2018-05-04 18:47:31 +0900

    first upload

--- pytorch_dnn_basic.py
+++ pytorch_dnn_basic.py
@@ -2,13 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import cv2
 dtype = torch.cuda.FloatTensor
+
+# XOR Problems
+
 class dnn(nn.Module):
     def __init__(self):
         super().__init__()
         input_size = 2
-        Channel_0 = 20
-        Channel_1 = 20
+        Channel_0 = 10
+        Channel_1 = 10
         output = 1
         # self.weight0 = torch.cuda.FloatTensor(input_size, Channel_0)
         self.fc1 = nn.Linear(input_size,Channel_0)
@@ -18,7 +22,7 @@ class dnn(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.sigmoid(self.fc3(x))
         return x
 
 def make_data(length, dtype):
@@ -36,23 +40,22 @@ def make_data(length, dtype):
         elif data[i, 0] < 0.5 and data[i, 1] < 0.5:
             target[i] = 0
     return torch.from_numpy(data).type(dtype), torch.from_numpy(target).type(dtype)
+
+
 my_dnn = dnn()
 my_dnn = my_dnn.cuda()
-
 optimizer = torch.optim.Adam(my_dnn.parameters(), lr=0.01)
-data,target = make_data(1000, dtype)
+
+data,target = make_data(5000, dtype)
 test,test_target = make_data(50, dtype)
 
-# test = torch.from_numpy(test).type(dtype)
-
-# print(target)
-# print(data.shape)
-# data_tensor = torch.from_numpy(data[19*50:19*50+50,:])
-# print(data_tensor)
-# target =
+test_for_x1 =torch.from_numpy(np.linspace(0,1,100,dtype = np.float16)).type(dtype)
+test_for_x2 = torch.from_numpy(np.linspace(0,1,100,dtype = np.float16)).type(dtype)
+test_for_y = np.zeros((100,100,1),dtype=np.float16)
+# print(test_for_vis.shape)
 def train(epoch):
     my_dnn.train()
-    for ii in range(20):
+    for ii in range(100):
         data_tensor = data[ii*50:ii*50+50,:]
 
         target_tensor = target[ii*50:ii*50+50]
@@ -61,7 +64,7 @@ def train(epoch):
         loss = F.l1_loss(output,target_tensor)
         loss.backward()
         optimizer.step()
-        print(loss)
+        # print(loss)
 
         # print('Train Epoch: {} [ {}/{} ({:.0f}%)]\t Loss: {:.6f}'.format(epoch, (ii+1)*50, 1000 ,100.*ii/1000),loss(0))
 #     for
@@ -71,5 +74,17 @@ for epoch in range(1, 10+1):
     # print(my_dnn(test))
 
 
-out = my_dnn(test)>=0.5
-print((out.type(dtype) - test_target == 0.).sum().type(dtype)/50*100)
+# out = my_dnn(test)>=0.5
+# print((out.type(dtype) - test_target == 0.).sum().type(dtype)/50*100)
+for i in range(100):
+    for j in range(100):
+        test_for_y[i,j,0] = my_dnn(torch.FloatTensor([test_for_x1[i],test_for_x2[j]]).type(dtype)).data.cpu().numpy()
+        print(test_for_y[i,j,0])
+
+# print(test_for_y[i,j,0])
+test_for_y = test_for_y+test_for_y.min()
+test_for_y = test_for_y/test_for_y.max()
+test_for_y = test_for_y*255
+test_for_y = test_for_y.astype(np.uint8)
+cv2.imshow('win',test_for_y)
+cv2.waitKey(0)
